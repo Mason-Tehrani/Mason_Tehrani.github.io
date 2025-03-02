9f58a7c8806831bc264bc18702dbd9dbd17f0e9d
Mason-Tehrani <001241528@mohawkcollege.ca>
2025-03-02 18:33:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const links = document.querySelectorAll("nav a");
-
-    function setActiveLink() {
-        let scrollPosition = window.scrollY + 100; // Adjust offset
-
-        document.querySelectorAll("section").forEach((section) => {
-            if (
-                section.offsetTop <= scrollPosition &&
-                section.offsetTop + section.offsetHeight > scrollPosition
-            ) {
-                links.forEach((link) => link.classList.remove("active"));
-                document
-                    .querySelector(`nav a[href="#${section.id}"]`)
-                    .classList.add("active");
-            }
-        });
-    }
-
-    window.addEventListener("scroll", setActiveLink);
-});
 
 
 document.addEventListener("DOMContentLoaded", () => {
